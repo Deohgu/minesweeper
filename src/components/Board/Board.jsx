5a001d84cd8091f8ef0fae7c78f034f00db58ec5
Deohgu <dioggopsanttos@gmail.com>
2020-08-24 16:15:40 +0100

    Data to render Cells.

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 import { Cell } from "./../index";
 
-export const Board = () => {
-  return (
-    <>
-      <Cell />
-    </>
-  );
+import { BoardStyled } from "./Board.styled";
+
+export const Board = (props) => {
+  const boardGrid = [];
+  for (let i = 0; i < props.size; i++) {
+    boardGrid.push(<Cell key={Cell + i} />);
+  }
+
+  return <BoardStyled>{boardGrid.map((curr) => curr)}</BoardStyled>;
 };
---
+++ src/components/Board/Board.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const BoardStyled = styled.div`
+  display: flexbox;
+  flex-wrap: wrap;
+  width: 100vw;
+`;
