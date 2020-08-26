715f3ec458bf09e0bb81712d4cdda484141e00a4
Deohgu <dioggopsanttos@gmail.com>
2020-08-26 18:28:12 +0100

    Re-done grid & bomb populator.

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -5,13 +5,24 @@ import { Cell } from "./../index";
 import { BoardStyled } from "./Board.styled";
 
 export const Board = (props) => {
-  const cellGrid = [];
-  for (let i = 0; i < props.size; i++) {
-    cellGrid.push({
-      // testing by adding isBomb directly here. Will be replaced by a proper bomb populator
-      cell: <Cell key={Cell + i} isBomb={Math.round(Math.random())} />,
-    });
+  ///////////////////////// Creator of grid & Bomb Populator
+  const populatedGrid = [];
+  for (let i = 0; i < props.size - props.bombs; i++) {
+    populatedGrid.push("0");
   }
+  for (let j = 0; j < props.bombs; j++) {
+    populatedGrid.push("1");
+  }
+  const shuffledPopulatedGrid = populatedGrid.sort(
+    (a, b) => Math.random() - 0.5
+  );
+  /////////////////////////
 
-  return <BoardStyled>{cellGrid.map((curr) => curr.cell)}</BoardStyled>;
+  return (
+    <BoardStyled>
+      {shuffledPopulatedGrid.map((curr, index) => (
+        <Cell key={"Cell" + index} isBomb={curr} />
+      ))}
+    </BoardStyled>
+  );
 };
--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -6,10 +6,11 @@ import { GameStyled } from "./Game.styled";
 
 export const Game = () => {
   const [size, setSize] = useState(64);
+  const [bombs, setBombs] = useState(20);
 
   return (
     <GameStyled>
-      <Board size={size} />
+      <Board size={size} bombs={bombs} />
     </GameStyled>
   );
 };
