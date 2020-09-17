ad73e4d0c71afe7e547aabc18b3c498d1c1ab869
Deohgu <dioggopsanttos@gmail.com>
2020-09-17 23:09:21 +0100

    Minor cleaning.

--- src/components/Cell/Cell.jsx
+++ src/components/Cell/Cell.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { CellStyled } from "./Cell.styled";
 
 export const Cell = ({ gridWidth, value, ...rest }) => {
-  console.log(value);
+  // console.log(value);
 
   return (
     <CellStyled gridWidth={gridWidth} {...rest}>
