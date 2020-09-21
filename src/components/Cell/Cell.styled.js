7646717af4e378a5286478e609666e23733edc67
Deohgu <dioggopsanttos@gmail.com>
2020-09-21 15:47:01 +0100

    Button styling & soft coding the margin.

--- src/components/Cell/Cell.styled.js
+++ src/components/Cell/Cell.styled.js
@@ -1,13 +1,21 @@
 import styled from "styled-components";
 
+const margin = 1;
+
 export const CellStyled = styled.div`
   /* Have it modular, don't hard code the 8 or the 4. */
+  /* Centering numbers & bombs in Cell */
   display: flex;
   align-items: center;
   justify-content: center;
 
-  width: calc((100% / ${(props) => props.gridWidth}) - 4px);
-  height: calc((100% / ${(props) => props.gridWidth}) - 4px);
+  width: calc((100% / ${(props) => props.gridWidth}) - ${margin * 2}px);
+  height: calc((100% / ${(props) => props.gridWidth}) - ${margin * 2}px);
   background-color: lightgrey;
-  margin: 2px;
+  margin: ${margin}px;
+
+  box-shadow: ${(props) =>
+    props.pressed
+      ? "inset 1px 1px 1px 1px grey"
+      : "inset -1px -1px 1px 1px grey"};
 `;
