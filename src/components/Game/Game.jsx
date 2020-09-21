a499afe6bc2122e319c347fedc2b6a884a62776b
Deohgu <dioggopsanttos@gmail.com>
2020-09-21 23:23:25 +0100

    Lifting State & features.

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -2,16 +2,36 @@ import React, { useState } from "react";
 
 import { Board } from "./../index";
 
-import { GameStyled } from "./Game.styled";
+import { GameStyled, Reset, ScoreBoard, Paragraph } from "./Game.styled";
 
 export const Game = () => {
   const [gridWidth, setGridWidth] = useState(8);
   const [size, setSize] = useState(64);
   const [bombs, setBombs] = useState(10);
+  const [gameOver, setGameOver] = useState(false);
+
+  const gameOverHandler = () => {
+    if (gameOver === false) {
+      setGameOver(true);
+    } else {
+      setGameOver(false);
+    }
+  };
 
   return (
     <GameStyled>
-      <Board gridWidth={gridWidth} size={size} bombs={bombs} />
+      <ScoreBoard>
+        <Reset onClick={() => setGameOver(false)}>↻</Reset>
+        <Paragraph>Timer:</Paragraph>
+        <Paragraph>{gameOver === false ? "" : "Game Over!"}</Paragraph>
+      </ScoreBoard>
+      <Board
+        gridWidth={gridWidth}
+        size={size}
+        bombs={bombs}
+        gameOver={gameOver}
+        gameOverHandler={gameOverHandler}
+      />
     </GameStyled>
   );
 };
