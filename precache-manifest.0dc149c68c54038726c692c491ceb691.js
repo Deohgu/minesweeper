d6783f58c3523094bb4380fee60222dd24fabf1d
Deohgu <dioggopsanttos@gmail.com>
2020-09-25 19:02:17 +0100

    Updates

---
+++ precache-manifest.0dc149c68c54038726c692c491ceb691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "920a4b0159391fad7356cd36416595a5",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "7dbf19834563a962e499",
+    "url": "/minesweeper/static/js/2.03134dfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/minesweeper/static/js/2.03134dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1fd8b45c19fc09d84bf",
+    "url": "/minesweeper/static/js/main.4c93161a.chunk.js"
+  },
+  {
+    "revision": "03fdde6e651e9bdac82d",
+    "url": "/minesweeper/static/js/runtime-main.9c76a611.js"
+  }
+]);
