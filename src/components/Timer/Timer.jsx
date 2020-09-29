e96dfe8e3c38e5ea31767faa7ad7ff43929c7b80
Deohgu <dioggopsanttos@gmail.com>
2020-09-29 16:54:06 +0100

    Timer prototype working. Missing queue to run it.

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,21 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export const Timer = (props) => {
-  const [timerSeconds, setTimerSeconds] = useState(0.0);
+  const [timerSeconds, setTimerSeconds] = useState(0);
+
+  const millisecTime = () => new Date().getMilliseconds();
+
+  const counterRef = useRef(null);
 
   useEffect(() => {
-    if (props.gameOver === false) {
-      startTimer();
+    if (props.gameOver === true) {
+      clearInterval(counterRef.current);
     }
   }, [timerSeconds]);
 
-  console.log(props.gameOver);
+  useEffect(() => {
+    startTimer();
+  }, []);
+
+  // console.log(`new Date(): ${today}`);
+
+  console.log(`timerSeconds: ${timerSeconds}`);
+
+  // console.log(`Game Over? ${props.gameOver}`);
 
   const startTimer = () => {
-    setTimeout(() => {
+    counterRef.current = setInterval(() => {
       // Need to use the Date object, this way is veryy inacurate when using miliseconds.
-      setTimerSeconds(timerSeconds + 1);
-    }, 1000);
+      setTimerSeconds((timerSeconds) =>
+        Number((timerSeconds + 0.01).toPrecision(4))
+      );
+    }, 10);
   };
 
   return timerSeconds;
