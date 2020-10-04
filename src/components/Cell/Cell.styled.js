3cb8eed8d29b3e3cd97ed66c8497b0e23c64e1d6
Deohgu <dioggopsanttos@gmail.com>
2020-10-04 23:30:23 +0100

    Softer colours.

--- src/components/Cell/Cell.styled.js
+++ src/components/Cell/Cell.styled.js
@@ -3,6 +3,9 @@ import styled from "styled-components";
 const margin = 1;
 
 export const CellStyled = styled.div`
+  font-size: 25px;
+  font-weight: 600;
+
   /* Have it modular, don't hard code the 8 or the 4. */
   /* Centering numbers & bombs in Cell */
   display: flex;
@@ -12,11 +15,11 @@ export const CellStyled = styled.div`
   width: calc((100% / ${(props) => props.gridWidth}) - ${margin * 2}px);
   height: calc((100% / ${(props) => props.gridWidth}) - ${margin * 2}px);
   background-color: ${(props) =>
-    props.pressed ? "hsl(0deg 0% 77%)" : "hsl(0deg 0% 83%)"};
+    props.pressed ? "hsl(0deg 0% 77%)" : "hsl(0deg 0% 74%)"};
   margin: ${margin}px;
 
   box-shadow: ${(props) =>
     props.pressed
-      ? "inset 2px 2px 1px 1px grey"
-      : "inset -2px -2px 1px 1px grey"};
+      ? "inset 2px 2px 1px 1px hsl(0deg 0% 40%)"
+      : "inset -2px -2px 1px 1px hsl(0deg 0% 40%)"};
 `;
