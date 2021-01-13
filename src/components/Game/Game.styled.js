93dba3191ef21b148832064203a326e3592be5ea
Deohgu <dioggopsanttos@gmail.com>
2021-01-13 18:20:40 +0000

    🚧 🎨 Improving the sizing code and commenting.

--- src/components/Game/Game.styled.js
+++ src/components/Game/Game.styled.js
@@ -2,28 +2,31 @@ import styled from "styled-components";
 
 export const GameStyled = styled.div`
   position: relative;
-  margin: 20% auto;
+  /* margin: 20% auto; */
   border: 15px ridge lightgrey;
-  width: 80vw;
-  height: calc(80vw + 12vw);
-  max-width: calc(700px * .8);
-  max-height: calc(700px * .92);
+  // 85% of its height. If changed here it needs to be changed in the child components (Scoreboard and board) as they have it hardcoded as 15% and 85%.
+  width: calc(80vh * 0.85);
+  height: 80vh;
+  max-width: calc(700px * 0.85);
+  max-height: calc(700px);
 
-  @media (min-width: 800px) {
-    margin: 5% auto;
+  @media (max-width: calc(700px * 0.85)) {
+    width: 100vw;
+    // 15% larger than the width to maintain the ratio
+    height: calc(100vw * 1.15);
+    /* margin: 5% auto; */
   }
 `;
 
 export const ScoreBoard = styled.div`
   display: flex;
-  flex-grow: 0;
   justify-content: space-between;
 
   font-size: calc(8vw);
   font-weight: 400;
   color: white;
 
-  width: 100%;
+  /* width: 100%; */
   height: 15%;
   background-color: hsl(0deg 0% 55%);
   @media (min-width: 800px) {
