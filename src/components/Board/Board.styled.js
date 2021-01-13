37427634ef3079788a90883fdf8840b43806a2a6
Deohgu <dioggopsanttos@gmail.com>
2021-01-13 18:19:42 +0000

    📝 🚧 Minimal comment to understand the sizing.

--- src/components/Board/Board.styled.js
+++ src/components/Board/Board.styled.js
@@ -6,5 +6,5 @@ export const BoardStyled = styled.div`
   justify-content: center;
   flex-wrap: wrap;
   width: 100%;
-  height: 85%;
+  height: 85%; // 85% as that is the size of the parents width. 85% of the height. So this component will be squared
 `;
