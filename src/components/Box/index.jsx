6019356707e966ba53f1f5d19d243139d7ab85e6
Deohgu <dioggopsanttos@gmail.com>
2021-01-14 13:58:04 +0000

    📝 Removed documentation.

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -2,9 +2,6 @@ import React from "react";
 
 import { Wrapper } from "./Box.styled";
 
-// Reading this.
-// https://medium.com/javascript-in-plain-english/how-to-build-reusable-layouts-in-react-js-daf8adcbca79
-
 export const Box = ({ children, ...rest }) => {
   return <Wrapper {...rest}>{children}</Wrapper>;
 };
