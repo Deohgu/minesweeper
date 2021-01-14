330c3ebd9b456cf365a2a63e61b89a6c494dd9f8
Deohgu <dioggopsanttos@gmail.com>
2021-01-14 13:39:48 +0000

    🐛 🎨  New styles and standardised initial values

--- src/components/Box/Box.styled.js
+++ src/components/Box/Box.styled.js
@@ -4,26 +4,32 @@ export const Wrapper = styled.div`
   // *Destructures* the received props from rest into the a variable that matches the name. No need for props.display
   display: ${({ display }) => (display ? display : "Flex")};
 
+  align-items: ${({ alignItems }) => (alignItems ? alignItems : "initial")};
+
   justify-content: ${({ justifyContent }) =>
     justifyContent ? justifyContent : "start"};
 
   flex-direction: ${({ flexDirection }) =>
     flexDirection ? flexDirection : "row"};
 
-  width: ${({ width }) => (width ? width : "100%")};
+  width: ${({ width }) => (width ? width : "initial")};
 
-  height: ${({ height }) => (height ? height : "fit-content")};
+  height: ${({ height }) => (height ? height : "initial")};
 
-  max-width: ${({ maxWidth }) => (maxWidth ? maxWidth : "100%")};
+  max-width: ${({ maxWidth }) => (maxWidth ? maxWidth : "initial")};
 
-  max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : "fit-content")};
+  max-height: ${({ maxHeight }) => (maxHeight ? maxHeight : "initial")};
 
   border: ${({ border }) => (border ? border : "initial")};
+
+  margin: ${({ margin }) => (margin ? margin : "initial")};
 
   font-size: ${({ fontSize }) => (fontSize ? fontSize : "inherit")};
 
   color: ${({ color }) => (color ? color : "inherit")};
 
+  line-height: ${({ lineHeight }) => (lineHeight ? lineHeight : "initial")};
+
   background-color: ${({ backgroundColor }) =>
-    backgroundColor ? backgroundColor : "inherit"};
+    backgroundColor ? backgroundColor : "initial"};
 `;
