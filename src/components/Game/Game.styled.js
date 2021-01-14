b383bd14b5185f7b709e6ec6d1391a3dea65fdab
Deohgu <dioggopsanttos@gmail.com>
2021-01-14 00:05:33 +0000

    🚧 🎨 Working with reusable component, cleaned sizing formulas

--- src/components/Game/Game.styled.js
+++ src/components/Game/Game.styled.js
@@ -1,58 +1,23 @@
 import styled from "styled-components";
 
-export const GameStyled = styled.div`
-  position: relative;
-  /* margin: 20% auto; */
-  border: 15px ridge lightgrey;
+import { Box } from "../Box";
+
+export const GameBox = styled(Box)`
+  flex-direction: column;
+  width: 100vw;
+  height: calc(100vw * 1.15);
   // 85% of its height. If changed here it needs to be changed in the child components (Scoreboard and board) as they have it hardcoded as 15% and 85%.
-  width: calc(80vh * 0.85);
-  height: 80vh;
   max-width: calc(700px * 0.85);
   max-height: calc(700px);
-
-  @media (max-width: calc(700px * 0.85)) {
-    width: 100vw;
-    // 15% larger than the width to maintain the ratio
-    height: calc(100vw * 1.15);
-    /* margin: 5% auto; */
-  }
+  border: 15px ridge lightgrey;
 `;
 
-export const ScoreBoard = styled.div`
-  display: flex;
+export const ScoreBoardBox = styled(Box)`
   justify-content: space-between;
-
-  font-size: calc(8vw);
-  font-weight: 400;
+  font-size: clamp(3px, 9.4vw, calc((700px * 0.85) * 0.094));
   color: white;
-
-  /* width: 100%; */
   height: 15%;
   background-color: hsl(0deg 0% 55%);
-  @media (min-width: 800px) {
-    font-size: 63.68px;
-  }
-`;
-
-export const TimerText = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  margin: auto;
-  /* width: calc(100% + 20%); */
-  width: 25vw;
-
-  @media (min-width: 800px) {
-    width: 200px;
-  }
-`;
-
-export const GameStatus = styled.div`
-  font-size: 30px;
-  position: absolute;
-  top: calc(50% + 30px + 16px);
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: yellow;
 `;
 
 export const IconGroup = styled.div`
@@ -62,7 +27,7 @@ export const IconGroup = styled.div`
 export const Icon = styled.div`
   transform: translateY(15%);
 
-  @media (min-width: 330px) {
+  @media (min-width: calc(700px * 0.85)) {
     margin: 5px;
   }
 `;
