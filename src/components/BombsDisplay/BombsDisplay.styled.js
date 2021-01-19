961a973a453839c91ec9aa009a6c16fc4914ded6
Deohgu <dioggopsanttos@gmail.com>
2021-01-19 15:32:50 +0000

    💄 Adds inset borders

--- src/components/BombsDisplay/BombsDisplay.styled.js
+++ src/components/BombsDisplay/BombsDisplay.styled.js
@@ -3,11 +3,14 @@ import styled from "styled-components";
 import { Box } from "../Box";
 
 export const BombsBox = styled(Box)`
+  box-sizing: border-box;
   align-items: center;
   justify-content: center;
   height: 100%;
-  background-color: black;
   padding: 0.5%;
+  border: inset 3px hsl(0 0% 92%);
+  background-color: black;
+  user-select: none;
 `;
 
 export const BombsImg = styled.img`
--- src/components/Timer/Timer.styled.js
+++ src/components/Timer/Timer.styled.js
@@ -3,15 +3,18 @@ import styled from "styled-components";
 import { Box } from "../Box/index";
 
 export const TimerBox = styled(Box)`
+  box-sizing: border-box;
   align-items: center;
   justify-content: center;
   height: 100%;
-  background-color: black;
   padding: 0.5%;
+  border: inset 3px hsl(0 0% 92%);
+  background-color: black;
 `;
 
 export const Counter = styled.img`
   height: calc(100% - 4px);
   margin: 0 2px;
   image-rendering: pixelated;
+  user-select: none;
 `;
