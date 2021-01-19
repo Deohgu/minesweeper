bfddcee510179570c10952535c15a15a589a62eb
Deohgu <dioggopsanttos@gmail.com>
2021-01-19 15:33:27 +0000

    💄 Reduces maximum app size

--- src/components/Game/Game.styled.js
+++ src/components/Game/Game.styled.js
@@ -7,10 +7,10 @@ export const GameBox = styled(Box)`
   width: 100vw;
   height: calc(100vw * 1.15);
   // 85% of its height. If changed here it needs to be changed in the child components (Scoreboard and board) as they have it hardcoded as 15% and 85%.
-  max-width: calc(700px);
+  max-width: calc(450px);
   max-height: calc(
-    700px + 700px * 0.15
+    450px + (450px * 0.15)
   ); // plus 15% in height of the total width for the scoreboard
-  border: 10px outset hsl(0deg 0% 92%);
+  border: 8px outset hsl(0deg 0% 92%);
   background-color: hsl(0deg 0% 74%);
 `;
