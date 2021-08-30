c74a659889c93267cd235fcd8e124d6c202aad2e
Diogo Santos <diogosantos@Diogos-MBP.broadband>
2021-08-30 16:52:43 +0100

    Moves files to meaningful location

--- src/components/Scoreboard/Scoreboard.jsx
+++ src/components/Scoreboard/Scoreboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BombsDisplay } from "../BombsDisplay/BombsDisplay";
+import { BombsDisplay } from "./BombsDisplay";
 
 import { Timer } from "../Timer/Timer";
 
