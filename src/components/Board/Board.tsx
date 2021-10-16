8915458a63f49c9560e3ab297e66cff93b454231
Diogo Santos <dioggopsanttos@gmail.com>
2021-10-16 19:59:35 +0100

    Moves Cell clicking logic down to Cell

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,74 +1,23 @@
-import React, { MouseEvent } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setCellArray,
-  setGameStatus,
-  incFlagsAvailable,
-  decFlagsAvailable,
-  cellType,
-  selectSettings,
-} from "../../store/settingsSlice";
+import React, { memo } from "react";
+import { useSelector } from "react-redux";
+import { selectSettings } from "../../store/settingsSlice";
 
 import { BoardStyled } from "./Board.styled";
 
-import { Cell } from "../Cell/Cell";
+import Cell from "../Cell/Cell";
 
-import { cellPressed } from "../../utils/BoardUtils/cellPressed";
-
-export const Board = () => {
-  const { gridColumns, gridSize, cellArray, gameStatus } =
-    useSelector(selectSettings);
-  const dispatch = useDispatch();
-
-  const flagHandler = (e: MouseEvent, index: number) => {
-    e.preventDefault();
-    if (gameStatus === "running") {
-      let cellArrayCopy = JSON.parse(JSON.stringify(cellArray));
-      if (cellArrayCopy[index].advancedChecked === false) {
-        if (cellArrayCopy[index].flagged === false) {
-          cellArrayCopy[index].flagged = true;
-          dispatch(decFlagsAvailable());
-        } else {
-          cellArrayCopy[index].flagged = false;
-          dispatch(incFlagsAvailable());
-        }
-      }
-      dispatch(setCellArray(cellArrayCopy));
-    }
-  };
-
-  const isClickableSquare = (curr: cellType) =>
-    (gameStatus === "waiting" || gameStatus === "running") &&
-    curr.flagged !== true &&
-    curr.advancedChecked === false;
+const Board = () => {
+  const { cellArray } = useSelector(selectSettings);
 
   return (
     <BoardStyled draggable="false">
       {cellArray.map((curr, index) => {
         return (
-          <Cell
-            onClick={() => {
-              // Should the onClick and onContextMenu logic be inside the Cell?
-              if (isClickableSquare(curr)) {
-                dispatch(
-                  setCellArray(
-                    cellPressed(index, cellArray, gridColumns, gridSize)
-                  )
-                );
-                if (gameStatus !== "running")
-                  dispatch(setGameStatus("running"));
-              }
-            }}
-            onContextMenu={(e: MouseEvent) => flagHandler(e, index)}
-            gridColumns={gridColumns}
-            pressed={curr.advancedChecked}
-            cellArray={cellArray}
-            index={index}
-            value={cellArray[index].value}
-            key={index}
-          />
+          <Cell index={index} pressed={curr.advancedChecked} key={index} />
         );
       })}
     </BoardStyled>
   );
 };
+
+export default memo(Board);
--- src/components/Cell/Cell.tsx
+++ src/components/Cell/Cell.tsx
@@ -1,39 +1,70 @@
-import React, { ComponentPropsWithRef } from "react";
-import { useSelector } from "react-redux";
+import React, { MouseEvent, ComponentPropsWithRef, memo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-  initialStateTypes,
-  cellType,
+  setCellArray,
+  setGameStatus,
+  incFlagsAvailable,
+  decFlagsAvailable,
   selectSettings,
 } from "../../store/settingsSlice";
 
 import { CellStyled } from "./Cell.styled";
 
 import { toDisplay } from "../../utils/CellUtils/toDisplay";
+import { cellPressed } from "../../utils/BoardUtils/cellPressed";
 
 type CellTypes = {
-  pressed: boolean;
-  cellArray: initialStateTypes["cellArray"];
   index: number;
-  value?: cellType["value"];
+  pressed: boolean;
 } & ComponentPropsWithRef<typeof CellStyled>;
 
-export const Cell = ({
-  pressed,
-  cellArray,
-  index,
-  value,
-  ...rest
-}: CellTypes) => {
-  const { gridColumns } = useSelector(selectSettings);
+const Cell = ({ index, pressed }: CellTypes) => {
+  const { gridSize, gridColumns, gameStatus, cellArray } =
+    useSelector(selectSettings);
+
+  const dispatch = useDispatch();
+
+  const flagHandler = (e: MouseEvent, index: number) => {
+    e.preventDefault();
+    if (gameStatus === "running") {
+      let cellArrayCopy = JSON.parse(JSON.stringify(cellArray));
+      if (cellArrayCopy[index].advancedChecked === false) {
+        if (cellArrayCopy[index].flagged === false) {
+          cellArrayCopy[index].flagged = true;
+          dispatch(decFlagsAvailable());
+        } else {
+          cellArrayCopy[index].flagged = false;
+          dispatch(incFlagsAvailable());
+        }
+      }
+      dispatch(setCellArray(cellArrayCopy));
+    }
+  };
+
+  const isClickableSquare = () =>
+    (gameStatus === "waiting" || gameStatus === "running") &&
+    cellArray[index].flagged !== true &&
+    cellArray[index].advancedChecked === false;
 
   return (
     <CellStyled
+      onClick={() => {
+        // Should the onClick and onContextMenu logic be inside the Cell?
+        if (isClickableSquare()) {
+          dispatch(
+            setCellArray(cellPressed(index, cellArray, gridColumns, gridSize))
+          );
+          if (gameStatus !== "running") dispatch(setGameStatus("running"));
+        }
+      }}
+      onContextMenu={(e: MouseEvent) => flagHandler(e, index)}
       gridColumns={gridColumns}
       pressed={pressed}
-      {...rest}
       draggable="false"
     >
-      {toDisplay(cellArray, index, value)}
+      {toDisplay(cellArray[index])}
     </CellStyled>
   );
 };
+
+export default memo(Cell);
